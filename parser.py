cb05895e8c45c19992e1241ac06079f289052956
infamicstudios <infamicstudios@gmail.com>
2018-11-27 19:23:23 -0800

    Fixed Turtle Stuff so it no longer uses Turtle

--- parser.py
+++ parser.py
@@ -1,167 +1,175 @@
 import turtle
+import NoTurtle
+import time
+import progressbar
+import time
+from numba import vectorize
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+from matplotlib import collections  as mc
+import pylab as pl
+
+#@vectorize(['int8(int8, int8)'], target='cuda')
 
 def lsystem(axioms, rules, iterations):
-    #    We iterate through our method required numbers of time.
-    for _ in range(iterations):
-        #    Our newly created axioms from this iteration.
-        newAxioms = ''
+	print("Generating instruction set")
+	#    We iterate through our method required numbers of time.
+	for _ in range(iterations):
+		#    Our newly created axioms from this iteration.
+		newAxioms = ''
 
-        #    This is your code, but with renamed variables, for clearer code.
-        for axiom in axioms:
-            if axiom in rules:
-                newAxioms += rules[axiom]
-            else:
-                newAxioms += axiom
-        #    You will need to iterate through your newAxioms next time, so...
-        #    We transfer newAxioms, to axioms that is being iterated on, in the for loop.
-        axioms = newAxioms
-    return axioms
+		#    This is your code, but with renamed variables, for clearer code.
+		for axiom in progressbar.progressbar(axioms):
+			if axiom in rules:
+				newAxioms += rules[axiom]
+			else:
+				newAxioms += axiom
+		#    You will need to iterate through your newAxioms next time, so...
+		#    We transfer newAxioms, to axioms that is being iterated on, in the for loop.
+		axioms = newAxioms
+	return axioms
 
 
 def drawLsystem(aTurtle,instructions,angle,distance, scaleFactor = 1):
 
-    commandInverse = False
-    Xcoordinates = [0]
-    Ycoordinates = [0]
-    coordinates = []
+	commandInverse = False
+	Xcoordinates = [0]
+	Ycoordinates = [0]
+	coordinates = []
 
-    aTurtle.penup()
-    aTurtle.setposition(0, 0)
-    aTurtle.pendown()
+	aTurtle.penup()
+	aTurtle.setposition(0, 0)
+	aTurtle.pendown()
 
-    for cmd in instructions:
-        if cmd == 'F':
-            aTurtle.forward(distance)
-        elif cmd == 'f':
-            aTurtle.penup()
-            aTurtle.forward(distance)
-            aTurtle.pendown()
-        elif cmd == 'B':
-            aTurtle.backward(distance)
-        elif cmd == '-' and commandInverse == False:
-            aTurtle.right(angle)
-        elif cmd == '-' and commandInverse == True:
-            aTurtle.left(angle)
-        elif cmd == '+' and commandInverse == False:
-            aTurtle.left(angle)
-        elif cmd == '+' and commandInverse == True:
-            aTurtle.right(angle)
-        elif cmd == '|':
-            aTurtle.left(180)
-        elif cmd == '{':
-            aTurtle.fill(True)
-            aTurtle.begin_poly()
+	print("Performing Math operations")
+	for cmd in progressbar.progressbar(instructions):
+		if cmd == 'F':
+			aTurtle.forward(distance)
+		elif cmd == 'f':
+			aTurtle.penup()
+			aTurtle.forward(distance)
+			aTurtle.pendown()
+		elif cmd == 'B':
+			aTurtle.backward(distance)
+		elif cmd == '-' and commandInverse == False:
+			aTurtle.right(angle)
+		elif cmd == '-' and commandInverse == True:
+			aTurtle.left(angle)
+		elif cmd == '+' and commandInverse == False:
+			aTurtle.left(angle)
+		elif cmd == '+' and commandInverse == True:
+			aTurtle.right(angle)
+		elif cmd == '|':
+			aTurtle.left(180)
+		elif cmd == '{':
+			aTurtle.fill(True)
+			aTurtle.begin_poly()
 
-        elif cmd == '}':
-            aTurtle.fill(False)
-            aTurtle.end_poly()
-        elif cmd == '@':
-            aTurtle.dot((distance/2),"black")
-        elif cmd == '>':
-            distance = distance * scaleFactor
-        elif cmd == '<':
-            distance = distance * scaleFactor
-        elif cmd == '&':
-            if commandInverse == True:
-                commandInverse = False
-            else: 
-                commandInverse = True
-        elif cmd == "[":
-            coordinates.append((aTurtle.xcor(),aTurtle.ycor())) 
-        elif cmd == "]":
-            aTurtle.penup()
-            aTurtle.setposition(coordinates[-1][0], coordinates[-1][1])
-            coordinates.pop()
-            aTurtle.pendown()
-        Xcoordinates.append(aTurtle.xcor()) 
-        Ycoordinates.append(aTurtle.ycor())
-    return (max(Xcoordinates)-min(Xcoordinates),max(Ycoordinates)-min(Ycoordinates))
+		elif cmd == '}':
+			aTurtle.fill(False)
+			aTurtle.end_poly()
+		elif cmd == '@':
+			aTurtle.dot((distance/2),"black")
+		elif cmd == '>':
+			distance = distance * scaleFactor
+		elif cmd == '<':
+			distance = distance * scaleFactor
+		elif cmd == '&':
+			if commandInverse == True:
+				commandInverse = False
+			else: 
+				commandInverse = True
+		elif cmd == "[":
+			coordinates.append((aTurtle.xcor(),aTurtle.ycor())) 
+		elif cmd == "]":
+			aTurtle.penup()
+			aTurtle.setposition(coordinates[-1][0], coordinates[-1][1])
+			coordinates.pop()
+			aTurtle.pendown()
+		Xcoordinates.append(aTurtle.xcor()) 
+		Ycoordinates.append(aTurtle.ycor())
+	return (max(Xcoordinates)-min(Xcoordinates),max(Ycoordinates)-min(Ycoordinates))
 
 
-def plotLsystem(aTurtle,instructions,angle,distance, scaleFactor = 1):
+def plotLsystem(fakeTurtle,instructions,angle,distance, scaleFactor = 1):
 
-    commandInverse = False
-    Xcoordinates = [0]
-    Ycoordinates = [0]
-    coordinates = []
-    returnedCoordinate = []
+	commandInverse = False
+	Xcoordinates = [0]
+	Ycoordinates = [0]
+	coordinates = []
+	returnedCoordinate = []
+	print("\n Performing Math operations \n")
+	for cmd in  progressbar.progressbar(instructions):
+		if cmd == 'F':
+			fakeTurtle.forward(distance)
+		elif cmd == 'B':
+			fakeTurtle.backward(distance)
+		elif cmd == '-' and commandInverse == False:
+			fakeTurtle.right(angle)
+		elif cmd == '-' and commandInverse == True:
+			fakeTurtle.left(angle)
+		elif cmd == '+' and commandInverse == False:
+			fakeTurtle.left(angle)
+		elif cmd == '+' and commandInverse == True:
+			fakeTurtle.right(angle)
+		elif cmd == '|':
+			fakeTurtle.left(180)
+		#elif cmd == '{':
+			#fakeTurtle.fill(True)
+			#fakeTurtle.begin_poly()
 
-    aTurtle.penup()
-    aTurtle.setposition(0, 0)
-    aTurtle.pendown()
-
-    for cmd in instructions:
-        if cmd == 'F':
-            aTurtle.forward(distance)
-        elif cmd == 'f':
-            aTurtle.penup()
-            aTurtle.forward(distance)
-            aTurtle.pendown()
-        elif cmd == 'B':
-            aTurtle.backward(distance)
-        elif cmd == '-' and commandInverse == False:
-            aTurtle.right(angle)
-        elif cmd == '-' and commandInverse == True:
-            aTurtle.left(angle)
-        elif cmd == '+' and commandInverse == False:
-            aTurtle.left(angle)
-        elif cmd == '+' and commandInverse == True:
-            aTurtle.right(angle)
-        elif cmd == '|':
-            aTurtle.left(180)
-        elif cmd == '{':
-            aTurtle.fill(True)
-            aTurtle.begin_poly()
-
-        elif cmd == '}':
-            aTurtle.fill(False)
-            aTurtle.end_poly()
-        elif cmd == '@':
-            aTurtle.dot((distance/2),"black")
-        elif cmd == '>':
-            distance = distance * scaleFactor
-        elif cmd == '<':
-            distance = distance * scaleFactor
-        elif cmd == '&':
-            if commandInverse == True:
-                commandInverse = False
-            else: 
-                commandInverse = True
-        elif cmd == "[":
-            coordinates.append((aTurtle.xcor(),aTurtle.ycor())) 
-        elif cmd == "]":
-            aTurtle.penup()
-            aTurtle.setposition(coordinates[-1][0], coordinates[-1][1])
-            coordinates.pop()
-            aTurtle.pendown()
-        
-        returnedCoordinate.append([(Xcoordinates[-1],Ycoordinates[-1]),(aTurtle.xcor(), aTurtle.ycor())])
-        Xcoordinates.append(aTurtle.xcor())
-        Ycoordinates.append(aTurtle.ycor())
-    aTurtle.reset()
-    return (returnedCoordinate, (max(Xcoordinates)-min(Xcoordinates),max(Ycoordinates)-min(Ycoordinates)))
+		#elif cmd == '}':
+			#fakeTurtle.fill(False)
+			#fakeTurtle.end_poly()
+		#elif cmd == '@':
+			#fakeTurtle.dot((distance/2),"black")
+		elif cmd == '>':
+			distance = distance * scaleFactor
+		elif cmd == '<':
+			distance = distance * scaleFactor
+		elif cmd == '&':
+			if commandInverse == True:
+				commandInverse = False
+			else: 
+				commandInverse = True
+		elif cmd == "[":
+			coordinates.append((fakeTurtle.xcor(),fakeTurtle.ycor())) 
+		elif cmd == "]":
+			fakeTurtle.setpos(coordinates[-1][0], coordinates[-1][1])
+			coordinates.pop()
+		
+		returnedCoordinate.append([(Xcoordinates[-1],Ycoordinates[-1]),(fakeTurtle.xcor(), fakeTurtle.ycor())])
+		Xcoordinates.append(fakeTurtle.xcor())
+		Ycoordinates.append(fakeTurtle.ycor())
+	return (returnedCoordinate, (max(Xcoordinates)-min(Xcoordinates),max(Ycoordinates)-min(Ycoordinates)))
 
 def main(plot):
-    t = turtle.Turtle()            # create the turtle
-    wn = turtle.Screen()
-    t.up()
-    t.back(200)
-    t.down()
-    t.speed(0)
+	fakeTurtle = NoTurtle.UndrawnTurtle()            # create the turtle
 
-    rules = { "X" : "X+YF++YF-FX--FXFX-YF+", "Y" : "-FX+YFYF++YF+FX--FX-Y" }
-    i = 4
-    while True:
-        instructions = lsystem('XF', rules, i)
-        
-        if plot:
-            from plot import performPlot
-            plotset = plotLsystem(t, instructions, 60, 200*(1/3)**i)
-            performPlot(plotset)
-        
-        else:
-            print(drawLsystem(t, instructions, 60, 200*(1/3)**i))
-        i += 1
-    wn.exitonclick()
+	rules = { "F" : "F++F++F|F-F++F" }
+	i = 7
+	while True:
+		instructions = lsystem('F++F++F++F++F', rules, i)
+		
+		if plot:
+			from plot import performPlot
+			plotset = plotLsystem(fakeTurtle, instructions, 36, 1000*(1/3)**i)
+			print("Generating plot")
+			start_time = time.time()
+			performPlot(plotset)
+			print("--- %s seconds ---" % (time.time() - start_time))
+			fig, ax = pl.subplots()
+			ax.add_collection(lc)
+			ax.axis('equal')
+			ax.autoscale()
+			ax.margins(0.1)
+			plt.savefig('koch.png', transparent=True)
+			plt.show()
+
+
+		else:
+			print(drawLsystem(fakeTurtle, instructions, 36, 1000*(1/3)**i))
+		i += 1
+	wn.exitonclick()
 
 main(True)
--- plot.py
+++ plot.py
@@ -3,36 +3,26 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 from matplotlib import collections  as mc
 import pylab as pl
+from numba import jit
 
+@cuda.jit
 def newline(p1, p2):
-    ax = plt.gca()
-    xmin, xmax = ax.get_xbound()
+	ax = plt.gca()
+	xmin, xmax = ax.get_xbound()
 
-    if(p2[0] == p1[0]):
-        xmin = xmax = p1[0]
-        ymin, ymax = ax.get_ybound()
-    else:
-        ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
-        ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
+	if(p2[0] == p1[0]):
+		xmin = xmax = p1[0]
+		ymin, ymax = ax.get_ybound()
+	else:
+		ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
+		ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
 
-    l = mlines.Line2D([xmin,xmax], [ymin,ymax])
-    ax.add_line(l)
-    return l
+	l = mlines.Line2D([xmin,xmax], [ymin,ymax])
+	ax.add_line(l)
+	return l
 
 def performPlot(plotset):
-    plotsetplots = plotset[0]
-    del plotsetplots[:2]
-    lc = mc.LineCollection(plotsetplots, linewidths=2)
-    fig, ax = pl.subplots()
-    ax.add_collection(lc)
-    ax.axis('equal')
-    ax.autoscale()
-    ax.margins(0.1)
-    i = 0
-    #for key in plotset[0]:
-        #[key[0],key[1]]  , [plotset[0][key][0],plotset[0][key][1]]
-    newline([0,0],[10,10])
-    plt.savefig('koch.png', transparent=True)
-    plt.show()
-    
-
+	plotsetplots = plotset[0]
+	del plotsetplots[:2]
+	lc = mc.LineCollection(plotsetplots, linewidths=2)
+	return lc
